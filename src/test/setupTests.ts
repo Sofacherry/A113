1c556976407c5a7dd52aae2581192bb352ef628c
Sofacherry <sofya.mokeeva.2005@mail.ru>
2026-04-30 12:38:22 +0300

    Add files via upload

---
+++ src/test/setupTests.ts
@@ -0,0 +1,39 @@
+﻿import '@testing-library/jest-dom/vitest';
+import { afterEach, vi } from 'vitest';
+import { cleanup } from '@testing-library/react';
+
+afterEach(() => {
+  cleanup();
+  if (typeof localStorage !== 'undefined') {
+    localStorage.clear();
+  }
+  vi.restoreAllMocks();
+});
+
+if (typeof window !== 'undefined') {
+  if (!window.matchMedia) {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: (query: string) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: () => {},
+        removeListener: () => {},
+        addEventListener: () => {},
+        removeEventListener: () => {},
+        dispatchEvent: () => false,
+      }),
+    });
+  }
+
+  class ResizeObserverMock {
+    observe() {}
+    unobserve() {}
+    disconnect() {}
+  }
+
+  if (!(window as any).ResizeObserver) {
+    (window as any).ResizeObserver = ResizeObserverMock;
+  }
+}
